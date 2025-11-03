72a95fa4a6456b26800370a0fcdc3b9e14c9bb92
SpeedxzpDev <speedkkzgui@gmail.com>
2025-11-03 07:49:00 -0300

    Melhoria em um comando.

--- src/commands/caraOuCoroa.js
+++
@@ -1,5 +0,0 @@
-const { SlashCommandBuilder } = require("discord.js");
-
-module.exports = {
-  data: new SlashCommandBuilder().setName("cara-ou-coroa").setDescription("Aposte com seu amigo, cara ou coroa!").addUserOption(option => option)
-}
