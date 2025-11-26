eef35b0b8ba2d2c3f3fab1b16511820f30fd8bdd
SpeedxzpDev <speedkkzgui@gmail.com>
2025-11-26 19:51:05 -0300

    New activies

--- src/events/on_ready.js
+++ src/events/on_ready.js
@@ -3,6 +3,14 @@ module.exports = (client) => {
   client.once("clientReady", () => {
   console.log(`Online como: ${client.user.username}`)
   console.log(`Latência do discord: ${client.ws.ping}ms`)
+  
+  client.user.setPresence({
+    activities: [{
+      name: "Bleach tybw",
+      type: 3
+    }],
+    status: "online"
+  });
 });
   
 }
